c6a7663c911256f6d38d77668f0fa6093f861a39
Javi Salazar <javisalazar2@gmail.com>
2020-06-11 21:28:11 -0500

    Update dataCleaner-4channels.py

--- zero-cross-automation/dataCleaner-4channels.py
+++ zero-cross-automation/dataCleaner-4channels.py
@@ -8,6 +8,6 @@ from scipy.fft import fft
 df = pd.read_csv(sys.argv[1], engine='c')
 tmpdf = df.loc[:, df.dtypes == np.float64]
 tmpdf = tmpdf.drop("F",axis = 1)
-tmpdf = tmpdf.drop("K",axis = 1)
+tmpdf = tmpdf.drop("L",axis = 1)
 tmpdf = tmpdf.drop("R",axis = 1)
 tmpdf.to_csv('cleanData.csv')

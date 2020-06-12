c7fb80045d5a8e01bf18a3839ef0bc2584714f25
Javi Salazar <javisalazar2@gmail.com>
2020-06-12 00:42:59 -0500

    Create dataCleaner-4channels.py

---
+++ zero-cross-automation/first-release/dataCleaner-4channels.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.fft import fft
+
+df = pd.read_csv(sys.argv[1], engine='c')
+tmpdf = df.loc[:, df.dtypes == np.float64]
+tmpdf = tmpdf.drop("F",axis = 1)
+tmpdf = tmpdf.drop("L",axis = 1)
+tmpdf = tmpdf.drop("R",axis = 1)
+tmpdf.to_csv('cleanData.csv')

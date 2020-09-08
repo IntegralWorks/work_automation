67fa9c2609fc17a2e1c9c788a8d606cbf4b13614
Javi Salazar <javisalazar2@gmail.com>
2020-09-08 08:47:48 -0500

    Update setLabelsAndScale.py

--- VISA_AUTOMATION/setLabelsAndScale.py
+++ VISA_AUTOMATION/setLabelsAndScale.py
@@ -21,9 +21,19 @@ f.write(scope.query(f'ch4:label?'))
 f.write(scope.query(f'ch4:scale?'))
 f.close()
 
+#Note: violated the DRY principle for better performance. I don't really recommend refactoring
+#to a for loop where you specify which channels to change because it really is faster this way.
+#but I do recognize not to code like this 99% of the time.
 if sys.argv[1] == '-s' or sys.argv[1] == '-simple':
-    print('feature not implemented yet')
-    #eventually
+    scope.write(f'ch1:label "{input("Type CH1 label: ")}" ')
+    scope.write(f'ch1:scale {input("Type CH1 voltage scale: ")} ')
+    scope.write(f'ch2:label "{input("Type CH2 label: ")}" ')
+    scope.write(f'ch2:scale {input("Type CH2 voltage scale: ")} ')
+    scope.write(f'ch3:label "{input("Type CH3 label: ")}" ')
+    scope.write(f'ch3:scale {input("Type CH3 voltage scale: ")} ')
+    scope.write(f'ch4:label "{input("Type CH4 label: ")}" ')
+    scope.write(f'ch4:scale {input("Type CH4 voltage scale: ")} ')
+
 elif sys.argv[1] == '-l' or sys.argv[1] == '-load':
     f2 = open(f'{sys.argv[2]}', 'r')
     newlineChar = "\n"
@@ -36,6 +46,7 @@ elif sys.argv[1] == '-l' or sys.argv[1] == '-load':
     scope.write(f'ch4:label {f2.readline().replace(newlineChar,"")}')
     scope.write(f'ch4:scale {f2.readline().replace(newlineChar,"")}')
     f2.close()
+
 else:
 #arguments go as {label voltageScaling}
 #therefore py thiscript.py -arg labelCH1 vScalingCH1 labelCH2 vScalingCH2 ...
@@ -47,3 +58,6 @@ else:
     scope.write(f'ch3:scale {sys.argv[7]}')
     scope.write(f'ch4:label "{sys.argv[8]}"')
     scope.write(f'ch4:scale {sys.argv[9]}')
+
+scope.close()
+rm.close()
